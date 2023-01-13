300f6ea812ffe076582907fad580181a46e21f56
Yeh37 <110619321+Yeh37@users.noreply.github.com>
2023-01-13 00:02:42 -0300

    Ajustes

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const port = 5000;
 // Body parser
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/", (req, res) => {
+  res.send("Welcome to a basic express App");
+});
+
+app.get(":ativo", async (req, res) => {
+  const ativo = req.params.ativo;
+
+  const BASE_URL = 'https://query2.finance.yahoo.com/v8';
+  const result = await axios.get(`${BASE_URL}/finance/chart/${ativo}`);
+
+  console.log('result', result.data);
+
+  res.json(result.data);
+});
+
 // Mock API
 app.get("/finance/chart/:ativo", async (req, res) => {
   const ativo = req.params.ativo;
