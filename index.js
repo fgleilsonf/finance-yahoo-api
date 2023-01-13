5e45a696e5e49a602e4aaa82e18f86cd983f95ef
Yeh37 <110619321+Yeh37@users.noreply.github.com>
2023-01-12 23:53:00 -0300

    Result api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
 
 const port = 5000;
@@ -6,26 +7,19 @@ const port = 5000;
 // Body parser
 app.use(express.urlencoded({ extended: false }));
 
-// Home route
-app.get("/", (req, res) => {
-  res.send("Welcome to a basic express App");
-});
-
 // Mock API
-app.get("/users", (req, res) => {
-  res.json([
-    { name: "William", location: "Abu Dhabi" },
-    { name: "Chris", location: "Vegas" }
-  ]);
-});
+app.get("/finance/chart/:ativo", async (req, res) => {
+  const ativo = req.params.ativo;
 
-app.post("/user", (req, res) => {
-  const { name, location } = req.body;
+  const BASE_URL = 'https://query2.finance.yahoo.com/v8';
+  const result = await axios.get(`${BASE_URL}/finance/chart/${ativo}`);
 
-  res.send({ status: "User created", name, location });
+  console.log('result', result.data);
+
+  res.json(result.data);
 });
 
 // Listen on port 5000
 app.listen(port, () => {
-  console.log(`Server is booming on port 5000 Visit http://localhost:5000`);
+  console.log(`API DE TESTE`);
 });
