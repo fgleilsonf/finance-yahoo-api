239b99826c2123eed244a7a0be3bf6f399a44c6d
Yeh37 <110619321+Yeh37@users.noreply.github.com>
2023-01-12 23:45:03 -0300

    asdasd

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const app = express();
 const port = 5000;
 
 // Body parser
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 // Home route
-// app.get("/", (req, res) => {
-//   res.send("Welcome to a basic express App");
-// });
+app.get("/", (req, res) => {
+  res.send("Welcome to a basic express App");
+});
+
+// Mock API
+app.get("/users", (req, res) => {
+  res.json([
+    { name: "William", location: "Abu Dhabi" },
+    { name: "Chris", location: "Vegas" }
+  ]);
+});
+
+app.post("/user", (req, res) => {
+  const { name, location } = req.body;
+
+  res.send({ status: "User created", name, location });
+});
 
 // Listen on port 5000
 app.listen(port, () => {
